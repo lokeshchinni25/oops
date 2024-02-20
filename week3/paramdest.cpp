cd4676aa5c2694070c37b56e9c77bf9b03c6f2ef
lokeshchinni25 <158846012+lokeshchinni25@users.noreply.github.com>
2024-02-20 19:57:44 +0530

    Create paramdest.cpp

---
+++ week3/paramdest.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+using namespace std;
+class student{
+    private:
+        string fullname;
+        int rollnum;
+        double sempercentage;
+        string collegename;
+        int collegecode;
+    public:
+        student()
+        {
+            collegename = "MVGR";
+            collegecode = 33;
+        }
+        student(string name,double percentage)
+        {
+            collegename = "MVGR";
+            collegecode = 33;
+            fullname = name;
+            sempercentage = percentage;
+        }
+        ~student(){}
+        void getstudentdetails()
+        {
+            cout << "The student details :" << endl;
+            cout << "Name : " << fullname  << endl ;
+            cout << "Rollno : " << rollnum << endl ;
+            cout << "College : " << collegename << endl ;
+            cout << "College code : " << collegecode << endl ;
+            cout << "Sem percentage : " << sempercentage   << endl ;
+        }
+        void setstudentrollno(int r)
+        {
+            rollnum = r;
+        }
+};
+
+int main()
+{
+    student s;
+    string name;
+    double sem;
+    int roll;
+    cout << "Enter your name : ";
+    getline(cin,name);
+    cout << "Enter your Roll no : ";
+    cin >> roll;
+    cout << "Enter your percentage : ";
+    cin >> sem;
+    s={name,sem};
+    s.setstudentrollno(roll);
+    s.getstudentdetails();        
+           
+}
