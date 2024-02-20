95a442d25ba7388041c0ee03ad12fd54204b546c
lokeshchinni25 <158846012+lokeshchinni25@users.noreply.github.com>
2024-02-20 19:54:48 +0530

    Create header.cpp

---
+++ week3/header.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "boxArea.h"
+#include "boxVolume.h"
+
+int main()
+{
+    float l,b,h;
+    cout << "Enter the length , width and height : ";
+    cin >> l >> b >> h ;
+    boxArea(l,b,h);
+    boxVolume(l,b,h);
+}
